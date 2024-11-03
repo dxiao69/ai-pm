6cd278b01ff7dfc48ef6a2086f00da492af8389d
Ding Xiao <dxiao69@126.com>
2024-11-03 10:14:51 -0500

    Update supertoken.cs

--- supertoken.cs
+++ supertoken.cs
@@ -60,3 +60,64 @@ public class SuperTokenUpdater
         return handler.WriteToken(jwtToken);
     }
 }
+
+
+
+using System;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Cryptography;
+using System.Text;
+using Microsoft.IdentityModel.Tokens;
+using System.Collections.Generic;
+using System.Linq;
+
+public class SuperTokenUpdater
+{
+    public static string UpdateSuperToken(string privateKey, string thumbPrint, string clientId, string link)
+    {
+        // Convert the thumbprint to a byte array
+        var certOctets = Enumerable.Range(0, thumbPrint.Length / 2)
+                                   .Select(i => Convert.ToByte(thumbPrint.Substring(i * 2, 2), 16))
+                                   .ToArray();
+
+        string x5t;
+        using (var sha1 = SHA1.Create())
+        {
+            var hash = sha1.ComputeHash(certOctets);
+            x5t = Convert.ToBase64String(hash).Replace("=", "").Replace("+", "-").Replace("/", "_");
+        }
+
+        // Create the signing key
+        var keyBytes = Convert.FromBase64String(privateKey);
+        var rsa = RSA.Create();
+        rsa.ImportRSAPrivateKey(keyBytes, out _);
+        var securityKey = new RsaSecurityKey(rsa) { KeyId = thumbPrint };
+
+        // Define the JWT payload
+        var payload = new JwtPayload
+        {
+            { "aud", link },
+            { "iss", clientId },
+            { "sub", clientId },
+            { "exp", DateTimeOffset.UtcNow.AddHours(24).ToUnixTimeSeconds() }
+        };
+
+        // Create the signing credentials
+        var signingCredentials = new SigningCredentials(securityKey, SecurityAlgorithms.RsaSha256);
+
+        // Create the JWT header with custom headers
+        var header = new JwtHeader(signingCredentials)
+        {
+            { "kid", thumbPrint },
+            { "x5t", x5t },
+            { "alg", "RS256" },
+            { "typ", "JWT" }
+        };
+
+        // Create the JWT token
+        var jwtToken = new JwtSecurityToken(header, payload);
+        var handler = new JwtSecurityTokenHandler();
+        return handler.WriteToken(jwtToken);
+    }
+}
+
